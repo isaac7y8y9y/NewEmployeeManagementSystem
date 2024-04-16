9204322d19d05f58ab1542fac9188d5e0dd45645
Isaac Yen <isaacyen7@gmail.com>
2024-04-16 09:50:50 -0700

    First Commit

---
+++ new_employee_management_system/employees/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Employee(models.Model):
+  id = models.IntegerField(primary_key=True)
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+  post = models.CharField(max_length=255)
+  salary = models.IntegerField(null=True)
+  joined_date = models.DateField(null=True)
+  # slug = models.SlugField(default="", null=False)
+
+  def __str__(self):
+    return f"{self.firstname} {self.lastname}"
---
+++ new_employee_management_system/employees/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Employee
+
+# This function retrieves all employees name and render all employees page
+def employees(request):
+  myemployees = Employee.objects.all().values()
+  template = loader.get_template('all_employees.html')
+  context = {
+    'myemployees': myemployees,
+  }
+  return HttpResponse(template.render(context, request))
+
+# This function retrieves each Employee by id and render employee details page
+def details(request, id):
+  myemployee = Employee.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'myemployee': myemployee,
+  }
+  return HttpResponse(template.render(context, request))
+
+# This functino render the main page
+def main(request):
+  template = loader.get_template('main.html')
+  return HttpResponse(template.render())
